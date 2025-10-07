0db570cda8548ed474131551033a12ba16ac695f
milat@JTLAPTOP <cdh8zs@virginia.edu>
2025-10-07 16:29:14 -0400

    Users App Created
    Created a Users app to handle our logins, features a basic login form for log in and sign up, no functionality yet just the static pages

---
+++ users/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def landing_page(request):
+    return render(request, 'users/login.html', {'mode': None})
+
+def login(request):
+    return render(request, 'users/login.html', {'mode': 'login'})
+
+def signup(request):
+    return render(request, 'users/login.html', {'mode': 'signup'})
