5eee5e5cf7bc65e1b52d1a1e25b36e67ae7b9b54
Eli Boyden <aqu4eb@virginia.edu>
2025-10-12 11:05:05 -0400

    updated styles and completed base layout

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def landing_page(request):
-    return render(request, 'users/login.html', {'mode': None})
+    return render(request, 'users/landing.html', {'mode': None})
 
 def login(request):
     return render(request, 'users/login.html', {'mode': 'login'})
