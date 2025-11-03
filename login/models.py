d888b6d2caa1403f93a4d80f835d482dffe54402
Oscar Ingram <oscardingram@gmail.com>
2025-11-03 13:35:02 -0500

    Consolidate profile models

--- login/models.py
+++ login/models.py
@@ -2,10 +2,3 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    is_suspended = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.user.username
--- users/models.py
+++ users/models.py
@@ -3,5 +3,9 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    is_suspended = models.BooleanField(default=False)
     image = models.ImageField(upload_to='profile_pics/')
+
+    def __str__(self):
+        return self.user.username
