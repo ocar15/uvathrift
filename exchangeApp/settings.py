db277522e14733cd16c6f33add2393b20a427451
potatoedbake <suk3ck@virginia.edu>
2025-11-03 18:11:24 -0500

    Added auto confirm on messages

--- exchangeApp/settings.py
+++ exchangeApp/settings.py
@@ -152,3 +152,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+POSTMAN_AUTO_MODERATE_AS = True
