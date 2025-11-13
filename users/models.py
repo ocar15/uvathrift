c0f7d6feb5b2330f75ca20a8fc4dd938f4ac5303
Oscar Ingram <oscardingram@gmail.com>
2025-11-12 21:24:26 -0500

    Auto-sized uploaded profile photos. Added profile photo to navbar

--- users/models.py
+++ users/models.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     is_suspended = models.BooleanField(default=False)
-    image = models.ImageField(upload_to='profile_pics/', default='default.jpg')
+    image = ProcessedImageField(upload_to='profile_pics',
+                                       processors=[ResizeToFill(200, 200)],
+                                       format='JPEG',
+                                       options={'quality': 60})
 
     def __str__(self):
         return self.user.username
