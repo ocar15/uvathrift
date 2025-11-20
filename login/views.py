5a5a05b017b48a290233c9c9f1b8a9f95e8734c5
milat@JTLAPTOP <cdh8zs@virginia.edu>
2025-11-20 10:11:05 -0500

    Started working on adding a appeals page and ensuring they can only submit one appeal. Next is to finish this then add the view for admins.

--- login/views.py
+++ login/views.py
@@ -21,6 +21,14 @@ def signup(request):
 def suspended(request):
     return render(request, 'login/suspended.html', {'mode': None})
 
+def appeal(request):
+    action = request.POST.get('action')
+
+    if action:
+        if action == 'submitAppeal':
+            pass #Add stuff here to save to an appeals model, have the text stored, and for other view only let them click button if they haven't already appealed.
+    return render(request, 'login/appeal.html', {'mode': None})
+
 def logout(request):
     user_logout(request)
     return redirect('/')
