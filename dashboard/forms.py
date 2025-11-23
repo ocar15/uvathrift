642894ea7e2ffe0a5b1a45246244013cd4128c22
Eli Boyden <aqu4eb@virginia.edu>
2025-11-23 15:14:02 -0500

    added create new listing form and styles, added back button on new listing page

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -5,3 +5,30 @@ class ItemForm(forms.ModelForm):
     class Meta:
         model = Item
         fields = ["title", "description", "price", "image"]
+
+        labels = {
+            "title": "",
+            "description": "",
+            "price": "",
+            "image": "",
+        }
+
+        widgets = {
+            "title": forms.TextInput(attrs={
+                "placeholder": "Title",
+                "class": "form-input",
+            }),
+            "description": forms.Textarea(attrs={
+                "placeholder": "Description",
+                "class": "form-textarea",
+                "rows": 4,
+            }),
+            "price": forms.NumberInput(attrs={
+                "placeholder": "Price",
+                "class": "form-input",
+                "step": "0.01",
+            }),
+            "image": forms.ClearableFileInput(attrs={
+                "class": "file-upload",
+            }),
+        }
