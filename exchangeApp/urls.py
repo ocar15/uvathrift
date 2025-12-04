784de901cafc663a9cc77241e4a2b3872e54200b
milat@JTLAPTOP <cdh8zs@virginia.edu>
2025-12-04 10:03:00 -0500

    Removed django admin page from urls

--- exchangeApp/urls.py
+++ exchangeApp/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path('mod/', include('moderation.urls')),
     path("", include('login.urls')),
     path("dashboard/", include('dashboard.urls')),
